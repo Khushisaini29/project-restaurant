2226c5e4c08b1b09984dbf7f51641570134d836d
Khushisaini29 <khushi.99960@gmail.com>
2026-09-14 20:21:58 +0530

    fix: resolve file casing conflict for OrderHistory

--- client/src/pages/Admin/OrderPage.jsx
+++ client/src/pages/Admin/OrderPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import OrderStore from "../../store/OrderStore";
-import OrderHistory from "./OrderHistory.jsx";
+import OrderHistory from "./ OrderHistory"
 
 const OrdersPage = () => {
   const { orderData, fetchAdminOrder ,OrderStatusChange} = OrderStore();
